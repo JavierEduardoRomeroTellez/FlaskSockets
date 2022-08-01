fd4a1c46418b3b4f6dbf84029bd566749833a425
Javier Eduardo Romero Tellez <romerotellezjavier@gmail.com>
2022-08-01 14:49:38 -0700

    Added 3 leds

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from gpiozero import LED
 
 led1 = LED(18)
 led_state1 = False
+led2 = LED(23)
+led_state2 = False
+led3 = LED(24)
+led_state3 = False
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -26,6 +30,34 @@ def turnOffLED():
     print("Apagar led")
     led1.off()
     led_state1 = False
+    send('False', broadcast = True)
+
+@socketio.on('turnOnLed2')
+def turnOnLED():
+    print("Encender led")
+    led2.on()
+    led_state2 = True
+    send('True', broadcast = True)
+
+@socketio.on('turnOffLed2')
+def turnOffLED():
+    print("Apagar led")
+    led2.off()
+    led_state2 = False
+    send('False', broadcast = True)
+
+@socketio.on('turnOnLed3')
+def turnOnLED():
+    print("Encender led")
+    led3.on()
+    led_state3 = True
+    send('True', broadcast = True)
+
+@socketio.on('turnOffLed3')
+def turnOffLED():
+    print("Apagar led")
+    led3.off()
+    led_state3 = False
     send('False', broadcast = True)
 
 if __name__ == '__main__':
